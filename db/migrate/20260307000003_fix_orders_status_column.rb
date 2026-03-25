72c9958b54cac4970569c6fb75181834291895bd
unknown <viejomoreno2@gmail.com>
2026-03-25 18:46:01 -0500

    Disable transaction for FixOrdersStatusColumn migration

--- db/migrate/20260307000003_fix_orders_status_column.rb
+++ db/migrate/20260307000003_fix_orders_status_column.rb
@@ -1,13 +1,7 @@
-# frozen_string_literal: true
-
-# Corrige la columna status: :bit no es válida en SQLite y puede hacer
-# que el default no se aplique (las órdenes nuevas aparecían cerradas).
 class FixOrdersStatusColumn < ActiveRecord::Migration[7.0]
-  def up
-    change_column :orders, :status, :integer, default: 0, null: false
-  end
+  disable_ddl_transaction!
 
-  def down
+  def change
     change_column :orders, :status, :integer, default: 0, null: false
   end
 end
