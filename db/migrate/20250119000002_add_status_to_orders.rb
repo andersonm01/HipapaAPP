ef6193eb4848d52f4cc83cdab16cb69a178e663c
unknown <viejomoreno2@gmail.com>
2026-03-25 18:41:55 -0500

    Fix postgres incompatible type

--- db/migrate/20250119000002_add_status_to_orders.rb
+++ db/migrate/20250119000002_add_status_to_orders.rb
@@ -1,5 +1,5 @@
 class AddStatusToOrders < ActiveRecord::Migration[7.0]
   def change
-    add_column :orders, :status, :bit, null: false, default: 0
+    add_column :orders, :status, :boolean, null: false, default: false
   end
 end
